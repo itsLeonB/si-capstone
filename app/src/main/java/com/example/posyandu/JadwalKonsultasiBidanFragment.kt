1e1d3a550be6bc744dd92af5320f483138be4393
Ellion Blessan <ellionblessan@gmail.com>
2023-12-14 02:14:39 +0700

    Update Jadwal Posyandu layout

--- app/src/main/java/com/example/posyandu/JadwalKonsultasiBidanFragment.kt
+++ app/src/main/java/com/example/posyandu/JadwalKonsultasiBidanFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [ChatFragment.newInstance] factory method to
+ * Use the [JadwalKonsultasiBidanFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ChatFragment : Fragment() {
+class JadwalKonsultasiBidanFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -31,10 +31,10 @@ class ChatFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false)
+        return inflater.inflate(R.layout.fragment_jadwal_konsultasi_bidan, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class ChatFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment ChatFragment.
+         * @return A new instance of fragment JadwalKonsultasiBidanFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            ChatFragment().apply {
+            JadwalKonsultasiBidanFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
